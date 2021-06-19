d647c9847812382793f72651eab839d386bf8647
Philipp Schaefer <pschaefer@posteo.net>
2021-06-19 10:31:46 +0200

    folder: components, routes; files: app, routes

---
+++ app/javascript/routes/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// import components here like
+// import Home from '../components/Home';
+
+export default (
+  <Router>
+    <Switch>
+      {/*define routes here like*/}
+      {/*<Route path='/' exact component={Home} />*/}
+    </Switch>
+  </Router>
+);
