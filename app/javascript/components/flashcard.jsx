14b1a5b861e75a028ca9be64274bdee48befc30a
Philipp Schaefer <pschaefer@posteo.net>
2021-07-21 19:36:34 +0200

    flashcards#show

--- app/javascript/components/flashcard.jsx
+++ app/javascript/components/flashcard.jsx
@@ -1,1 +1,33 @@
-flashcard.jsx
+import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+
+export default function Flashcard(props) {
+  const [flashcard, setFlashcard] = useState([]);
+  const id = props.match.params.id;
+
+  useEffect(() => {
+    const url = `/api/v1/flashcards/${id}`;
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(response => setFlashcard(response))
+      .catch(() => props.history.push('/'));
+  }, []);
+
+  const card = (
+    <div key={id}>
+      <p>{`${flashcard.question}`}</p>
+      <p>{`Solution: ${flashcard.solution}`}</p>
+    </div>
+  );
+
+  return (
+    <div>
+      {card}
+    </div>
+  );
+}
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Flashcards from '../components/flashcards';
+import Flashcard from '../components/flashcard';
 
 export default (
   <Router>
     <Switch>
       <Route path='/flashcards' exact component={Flashcards} />
+      <Route path='/flashcards/:id' exact component={Flashcard} />
     </Switch>
   </Router>
 );
